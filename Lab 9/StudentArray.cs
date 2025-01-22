12d405ea36662ad6c2485a93528ae99dfe892857
T1ray <tester2016go@gmail.com>
2025-01-22 14:01:12 +0500

    Добавлена часть 3, удален PrintStudent

---
+++ Lab 9/StudentArray.cs
@@ -0,0 +1,60 @@
+namespace Lab9_1;
+
+public class StudentArray
+{
+    // Переменные
+    private string[] randomNames = new string[]
+    {
+    "Александр",
+    "Сергей",
+    "Дмитрий",
+    "Елена",
+    "Андрей",
+    "Анастасия",
+    "Анна",
+    "Екатерина",
+    "Алексей",
+    "Наталья"
+    };
+    
+    // Свойства
+    public Student[] Students { get; set; }
+    // Индексатор 
+    
+    // Конструкторы
+    public StudentArray()
+    {
+        Students = new Student[1];
+        Students[0] = new Student("Вася", 19, 8.92);
+    }
+
+    public StudentArray(int numberStudents)
+    {
+        Students = new Student[numberStudents];
+        Random random = new Random();
+        for (int i = 0; i < numberStudents; i++)
+        {
+            Students[i] = new Student(randomNames[random.Next(0, randomNames.Length)], 
+                random.Next(18,23), random.NextDouble()*10);
+        }
+    }
+
+    public StudentArray(Student[] students)
+    {
+        Students = new Student[students.Length];
+        for (int i = 0; i < students.Length; i++)
+        {
+            Students[i] = students[i];
+        }
+    }
+    // Методы класса
+    public void PrintStudents()
+    {
+        for (int i = 0; i < Students.Length; i++)
+        {
+            Students[i].Info();
+        }
+    }
+    // Статические функции
+    
+}
